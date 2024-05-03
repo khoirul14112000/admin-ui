79cc7b0cd1ecb770afb9659eef6c160131e7d739
khoirul141100 <khoirulu818@gmail.com>
2024-05-04 00:35:13 +0700

    tugas2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,25 @@
 import "./home.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer"><Navbar/>container</div>
+      <div className="widgets">
+        <widget type="user" />
+        <widget type="order" />
+        <widget type="earning" />
+        <widget type="balance" />
+      </div>
+      <div className="charts">
+        <Chart type="order" aspect={2/1}/>
+        <Chart type="earning" aspect={2/1}/>
+      </div>    
+    </div>
+  );
+};
 
-export default Home
+export default Home;
